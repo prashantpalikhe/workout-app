429fe7154b450534998acef1fcde9761293f06d0
Prashant Palikhe <prashpal09@gmail.com>
2026-03-06 21:39:51 +0100

    feat: Scaffold Nuxt web

--- apps/web/app.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Workout App</h1>
-    <p>Ready to build.</p>
-  </div>
-</template>
--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,7 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/a11y',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxtjs/i18n',
+    '@pinia/nuxt',
+    '@nuxt/scripts'
+  ],
 
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true
+  },
 
-  compatibilityDate: '2025-01-01',
-});
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
